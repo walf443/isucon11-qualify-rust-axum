2c4ea00260bc213f6a943e2ce5d104082bb61a7f
Keiji Yoshimi <walf443@gmail.com>
2022-01-29 15:56:30 +0900

    added url field to TestApp.

--- tests/get_index.rs
+++ tests/get_index.rs
@@ -4,7 +4,7 @@ mod helper;
 async fn test_get_index() {
     let app = helper::spawn_app().await;
     let client = reqwest::Client::new();
-    let res = client.get(&format!("{}/", &app.address)).send().await
+    let res = client.get(app.url.join("/").unwrap()).send().await
         .expect("Failed to request");
 
     assert!(res.status().is_success());
--- tests/helper/mod.rs
+++ tests/helper/mod.rs
@@ -1,10 +1,12 @@
 use std::net::TcpListener;
+use reqwest::Url;
 use sqlx::Pool;
 use sqlx::MySql;
 use isucon11_qualify_rust_axum::{DBConfig, get_db_connection, run};
 
 pub struct TestApp {
     pub address: String,
+    pub url: Url,
     pub database: Pool<MySql>,
 }
 
@@ -17,5 +19,5 @@ pub async fn spawn_app() -> TestApp {
     let server = run(listener, pool.clone()).expect("Failed to bind to address");
     let _ = tokio::spawn(server);
     let address = format!("http://127.0.0.1:{port}");
-    TestApp { address: address, database: pool }
+    TestApp { address: address.clone(), database: pool, url: Url::parse(&address).unwrap() }
 }
