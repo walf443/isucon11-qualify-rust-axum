d8c1705f606ee0768bc5b5e4d7ef77dfa2b59792
Keiji Yoshimi <walf443@gmail.com>
2022-02-05 22:52:11 +0900

    implement post_authentication

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,10 +1,19 @@
 use crate::model::user_repository::{UserRepository, UserRepositoryImpl};
+use axum::extract::TypedHeader;
+use axum::headers::authorization::Bearer;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-use axum::{extract, Json};
+use axum::{extract, headers, Json};
 use sqlx::MySqlPool;
 use tower_cookies::{Cookie, Cookies};
 use tracing::log;
+
+const JIA_JWT_SIGNING_KEY_PATH: &str = "ec256-public.pem";
+
+lazy_static::lazy_static! {
+    static ref JIA_JWT_SIGNING_KEY_PEM: Vec<u8> = std::fs::read(JIA_JWT_SIGNING_KEY_PATH).expect("failed to read JIA JWT signing key file");
+    static ref JIA_JWT_SIGNING_KEY: jsonwebtoken::DecodingKey = jsonwebtoken::DecodingKey::from_ec_pem(&JIA_JWT_SIGNING_KEY_PEM).expect("failed to parse JIA JWT signing key");
+}
 
 pub mod initialize;
 pub mod isu;
@@ -25,11 +34,33 @@ pub async fn get_index() -> impl IntoResponse {
     (StatusCode::OK, Json(vec!["Hello, world"]))
 }
 
+#[derive(Debug, serde::Deserialize)]
+struct Claims {
+    jia_user_id: String,
+}
+
 pub async fn post_authentication(
     pool: extract::Extension<MySqlPool>,
+    TypedHeader(authorization): axum::extract::TypedHeader<headers::Authorization<Bearer>>,
     cookies: Cookies,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let jia_user_id = "1";
+    let req_jwt = authorization.token();
+
+    let validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::ES256);
+    let token = match jsonwebtoken::decode(req_jwt, &JIA_JWT_SIGNING_KEY, &validation) {
+        Ok(token) => token,
+        Err(e) => {
+            if matches!(e.kind(), jsonwebtoken::errors::ErrorKind::Json(_)) {
+                return Err((StatusCode::BAD_REQUEST, "invalid JWT payload".to_string()));
+            } else {
+                return Err((StatusCode::FORBIDDEN, "forbidden".to_string()));
+            }
+        }
+    };
+
+    let claims: Claims = token.claims;
+    let jia_user_id = claims.jia_user_id;
+
     let user_repo = UserRepositoryImpl { pool: pool.0 };
     user_repo
         .insert(jia_user_id.to_string())
