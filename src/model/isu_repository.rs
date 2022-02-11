f8c6e8ad892a36c49ee677eb0e9dd7f0081c5c6e
Keiji Yoshimi <walf443@gmail.com>
2022-02-11 16:31:56 +0900

    create isu_repository

---
+++ src/model/isu_repository.rs
@@ -0,0 +1,82 @@
+use crate::MySqlPool;
+use anyhow::Error;
+use async_trait::async_trait;
+
+pub struct Isu {
+    pub id: i64,
+    pub jia_user_id: String,
+    pub jia_isu_uuid: String,
+    pub name: String,
+    pub character: Option<String>,
+}
+
+#[async_trait]
+pub trait IsuRepository {
+    async fn find_all_by_user_id(&self, jia_user_id: String) -> Result<Vec<Isu>, sqlx::Error>;
+}
+
+#[derive(Clone)]
+pub struct IsuRepositoryImpl {
+    pub pool: MySqlPool,
+}
+
+#[async_trait]
+impl IsuRepository for IsuRepositoryImpl {
+    async fn find_all_by_user_id(&self, jia_user_id: String) -> Result<Vec<Isu>, sqlx::Error> {
+        let chairs = sqlx::query_as!(Isu, "SELECT id, jia_user_id, jia_isu_uuid, name, `character` FROM isu WHERE jia_user_id = ?", jia_user_id).fetch_all(&self.pool).await?;
+
+        Ok(chairs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::model::cleaner::tests::Cleaner;
+    use crate::model::isu_repository::{IsuRepository, IsuRepositoryImpl};
+    use crate::{get_db_connection, DBConfig};
+
+    #[tokio::test]
+    async fn test_find_all_by_user_id_empty() -> Result<(), sqlx::Error> {
+        let dbconf = DBConfig::default_for_test();
+        let pool = get_db_connection(&dbconf).await;
+
+        let mut cleaner = Cleaner::new(pool.clone());
+        cleaner.prepare_table("isu").await?;
+
+        let repo = IsuRepositoryImpl { pool: pool };
+        let result = repo.find_all_by_user_id("1".to_string()).await?;
+        assert_eq!(result.len(), 0);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_find_all_by_user_id_with_result() -> Result<(), sqlx::Error> {
+        let dbconf = DBConfig::default_for_test();
+        let pool = get_db_connection(&dbconf).await;
+
+        let mut cleaner = Cleaner::new(pool.clone());
+        cleaner.prepare_table("isu").await?;
+
+        sqlx::query!(
+            "INSERT INTO isu (jia_user_id, jia_isu_uuid, name) VALUES  (?,?,?), (?,?,?), (?,?,?)",
+            "1".to_string(),
+            "xxxx".to_string(),
+            "test1".to_string(),
+            "1".to_string(),
+            "yyyy".to_string(),
+            "test2".to_string(),
+            "1".to_string(),
+            "zzzz".to_string(),
+            "test3".to_string(),
+        )
+        .execute(&pool)
+        .await?;
+
+        let repo = IsuRepositoryImpl { pool: pool };
+        let result = repo.find_all_by_user_id("1".to_string()).await?;
+        assert_eq!(result.len(), 3);
+
+        Ok(())
+    }
+}
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,34 +1,41 @@
 use crate::model::isu_association_config_repository::{
     IsuAssociationConfigRepository, IsuAssociationConfigRepositoryImpl,
 };
+use crate::model::isu_repository::{IsuRepository, IsuRepositoryImpl};
 use crate::model::user_repository::{UserRepository, UserRepositoryImpl};
 use sqlx::MySqlPool;
 
 pub mod cleaner;
 pub mod isu_association_config_repository;
+pub mod isu_repository;
 pub mod user_repository;
 
 pub trait RepositoryManager: Clone + std::marker::Send + std::marker::Sync {
+    type IsuRepo: IsuRepository;
     type IsuAssociationConfigRepo: IsuAssociationConfigRepository;
     type UserRepo: UserRepository;
 
+    fn isu_repository(&self) -> &Self::IsuRepo;
     fn isu_accosiation_config_repository(&self) -> &Self::IsuAssociationConfigRepo;
     fn user_repository(&self) -> &Self::UserRepo;
 }
 
 #[derive(Clone)]
 pub struct RepositoryManagerImpl {
+    isu_repository: IsuRepositoryImpl,
     isu_accosication_config_repository: IsuAssociationConfigRepositoryImpl,
     user_repository: UserRepositoryImpl,
 }
 
 impl RepositoryManagerImpl {
     pub fn new(pool: MySqlPool) -> Self {
+        let isu_repoository = IsuRepositoryImpl { pool: pool.clone() };
         let isu_association_config_repository =
             IsuAssociationConfigRepositoryImpl { pool: pool.clone() };
         let user_repository = UserRepositoryImpl { pool: pool.clone() };
 
         Self {
+            isu_repository: isu_repoository,
             isu_accosication_config_repository: isu_association_config_repository,
             user_repository: user_repository,
         }
@@ -36,8 +43,13 @@ impl RepositoryManagerImpl {
 }
 
 impl RepositoryManager for RepositoryManagerImpl {
+    type IsuRepo = IsuRepositoryImpl;
     type IsuAssociationConfigRepo = IsuAssociationConfigRepositoryImpl;
     type UserRepo = UserRepositoryImpl;
+
+    fn isu_repository(&self) -> &Self::IsuRepo {
+        &self.isu_repository
+    }
 
     fn isu_accosiation_config_repository(&self) -> &Self::IsuAssociationConfigRepo {
         &self.isu_accosication_config_repository
