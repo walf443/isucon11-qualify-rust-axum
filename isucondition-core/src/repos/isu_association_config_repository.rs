ce3e1785ada8d142bebe7f26c32f917d7078c6cc
Keiji Yoshimi <walf443@gmail.com>
2022-02-12 13:59:51 +0900

    create wrap error

--- isucondition-core/src/repos/isu_association_config_repository.rs
+++ isucondition-core/src/repos/isu_association_config_repository.rs
@@ -1,11 +1,11 @@
-use anyhow::Error;
+use crate::repos::Result;
 use async_trait::async_trait;
 use sqlx::MySqlPool;
 
 #[async_trait]
 pub trait IsuAssociationConfigRepository {
-    async fn insert(&self, name: &str, url: &str) -> Result<(), anyhow::Error>;
-    async fn get_jia_service_url(&self) -> Result<String, anyhow::Error>;
+    async fn insert(&self, name: &str, url: &str) -> Result<()>;
+    async fn get_jia_service_url(&self) -> Result<String>;
 }
 
 #[derive(Clone)]
@@ -15,14 +15,14 @@ pub struct IsuAssociationConfigRepositoryImpl {
 
 #[async_trait]
 impl IsuAssociationConfigRepository for IsuAssociationConfigRepositoryImpl {
-    async fn insert(&self, name: &str, url: &str) -> Result<(), anyhow::Error> {
+    async fn insert(&self, name: &str, url: &str) -> Result<()> {
         sqlx::query!("INSERT INTO `isu_association_config` (`name`, `url`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `url` = VALUES(`url`)",
         name, url)
             .fetch_all(&self.pool).await?;
         Ok(())
     }
 
-    async fn get_jia_service_url(&self) -> Result<String, Error> {
+    async fn get_jia_service_url(&self) -> Result<String> {
         let result = sqlx::query!(
             "SELECT url FROM isu_association_config WHERE name = ?",
             "jia_service_url"
--- isucondition-core/src/repos/mod.rs
+++ isucondition-core/src/repos/mod.rs
@@ -4,3 +4,12 @@ pub mod isu_repository;
 pub mod user_repository;
 
 pub mod repository_manager;
+
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("failed to execute query")]
+    SqlError(#[from] sqlx::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
