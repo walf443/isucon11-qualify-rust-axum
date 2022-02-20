1e022c693b1d8f12f9c074cbcd699eca1d2213e3
Keiji Yoshimi <walf443@gmail.com>
2022-02-20 14:57:10 +0900

    test with None case

--- isucondition-core/src/repos/isu_repository.rs
+++ isucondition-core/src/repos/isu_repository.rs
@@ -1,7 +1,8 @@
 use crate::database::DBConnectionPool;
 use crate::models::isu::{Isu, IsuID, IsuUUID};
 use crate::models::user::UserID;
-use crate::repos::Result;
+use crate::repos::Error::SqlError;
+use crate::repos::{Error, Result};
 use async_trait::async_trait;
 
 #[cfg_attr(test, mockall::automock)]
@@ -45,21 +46,28 @@ impl IsuRepository for IsuRepositoryImpl {
         jia_uuid: &IsuUUID,
         jia_user_id: &UserID,
     ) -> Result<Option<Vec<u8>>> {
-        let record = sqlx::query!(
+        let result = sqlx::query!(
             "SELECT image FROM isu WHERE jia_isu_uuid = ? AND jia_user_id = ?",
             jia_uuid.to_string(),
             jia_user_id.to_string()
         )
         .fetch_one(&self.pool)
-        .await?;
+        .await;
 
-        Ok(record.image)
+        match result {
+            Ok(record) => Ok(record.image),
+            Err(err) => match err {
+                sqlx::error::Error::RowNotFound => Ok(None),
+                _ => Err(Error::SqlError(err)),
+            },
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::database::get_db_connection_for_test;
+    use crate::models::isu::IsuUUID;
     use crate::models::user::UserID;
     use crate::repos::isu_repository::{IsuRepository, IsuRepositoryImpl};
     use crate::repos::Result;
@@ -110,6 +118,28 @@ mod tests {
             .find_all_by_user_id(&UserID::new("1".to_string()))
             .await?;
         assert_eq!(result.len(), 3);
+
+        cleaner.clean().await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_find_image_by_uuid_and_user_id_with_empty() -> Result<()> {
+        let pool = get_db_connection_for_test().await;
+
+        let mut cleaner = Cleaner::new(pool.clone());
+        cleaner.prepare_table("isu").await?;
+
+        let repo = IsuRepositoryImpl { pool: pool };
+        let result = repo
+            .find_image_by_uuid_and_user_id(
+                &IsuUUID::new("test".to_string()),
+                &UserID::new("1".to_string()),
+            )
+            .await?;
+
+        assert!(result.is_none());
 
         cleaner.clean().await?;
 
