13b3bed1fb81b66e4e14eb4ff66ddbc73d2c2312
Keiji Yoshimi <walf443@gmail.com>
2022-02-20 19:09:33 +0900

    added test for find_by_uuid_and_user_id

--- isucondition-core/src/repos/isu_repository.rs
+++ isucondition-core/src/repos/isu_repository.rs
@@ -212,4 +212,57 @@ mod tests {
 
         Ok(())
     }
+
+    #[tokio::test]
+    async fn test_find_by_uuid_and_user_id_with_empty() -> Result<()> {
+        let pool = get_db_connection_for_test().await;
+
+        let mut cleaner = Cleaner::new(pool.clone());
+        cleaner.prepare_table("isu").await?;
+
+        let repo = IsuRepositoryImpl { pool: pool };
+        let result = repo
+            .find_by_uuid_and_user_id(
+                &IsuUUID::new("test".to_string()),
+                &UserID::new("1".to_string()),
+            )
+            .await?;
+
+        assert!(result.is_none());
+
+        cleaner.clean().await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_find_by_uuid_and_user_id_with_result() -> Result<()> {
+        let pool = get_db_connection_for_test().await;
+
+        let mut cleaner = Cleaner::new(pool.clone());
+        cleaner.prepare_table("isu").await?;
+
+        sqlx::query!(
+            "INSERT INTO isu (jia_user_id, jia_isu_uuid, name) VALUES  (?,?,?)",
+            "1".to_string(),
+            "test".to_string(),
+            "test1".to_string(),
+        )
+        .execute(&pool)
+        .await?;
+
+        let repo = IsuRepositoryImpl { pool: pool };
+        let result = repo
+            .find_by_uuid_and_user_id(
+                &IsuUUID::new("test".to_string()),
+                &UserID::new("1".to_string()),
+            )
+            .await?;
+
+        assert!(result.is_some());
+
+        cleaner.clean().await?;
+
+        Ok(())
+    }
 }
