577bb654b273096c026b38b4ebc77de651ad3f27
Keiji Yoshimi <walf443@gmail.com>
2022-02-22 23:33:02 +0900

    modulize by method name

--- isucondition-core/src/repos/user_repository.rs
+++ isucondition-core/src/repos/user_repository.rs
@@ -42,66 +42,75 @@ impl UserRepository for UserRepositoryImpl {
 
 #[cfg(test)]
 mod tests {
-    use crate::database::get_db_connection_for_test;
-    use crate::models::user::UserID;
-    use crate::repos::user_repository::{UserRepository, UserRepositoryImpl};
-    use crate::repos::Result;
-    use crate::test::Cleaner;
+    mod insert {
+        use crate::database::get_db_connection_for_test;
+        use crate::repos::user_repository::{UserRepository, UserRepositoryImpl};
+        use crate::repos::Result;
+        use crate::test::Cleaner;
 
-    #[tokio::test]
-    async fn test_user_repository_insert() -> Result<()> {
-        let pool = get_db_connection_for_test().await;
-        let mut cleaner = Cleaner::new(pool.clone());
-        cleaner.prepare_table("user").await?;
+        #[tokio::test]
+        async fn test_user_repository_insert() -> Result<()> {
+            let pool = get_db_connection_for_test().await;
+            let mut cleaner = Cleaner::new(pool.clone());
+            cleaner.prepare_table("user").await?;
 
-        let repo = UserRepositoryImpl { pool: pool.clone() };
-        let result = repo.insert("1".to_string()).await?;
-        let result = sqlx::query!(
-            "SELECT COUNT(*) as count FROM user WHERE jia_user_id = ?",
-            "1"
-        )
-        .fetch_one(&pool)
-        .await?;
-
-        assert_eq!(1, result.count);
-
-        cleaner.clean().await?;
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn test_user_repository_count_with_result() -> Result<()> {
-        let pool = get_db_connection_for_test().await;
-        let mut cleaner = Cleaner::new(pool.clone());
-        cleaner.prepare_table("user").await?;
-
-        sqlx::query!("INSERT INTO user (jia_user_id) VALUES (1)")
-            .execute(&pool)
+            let repo = UserRepositoryImpl { pool: pool.clone() };
+            let result = repo.insert("1".to_string()).await?;
+            let result = sqlx::query!(
+                "SELECT COUNT(*) as count FROM user WHERE jia_user_id = ?",
+                "1"
+            )
+            .fetch_one(&pool)
             .await?;
 
-        let repo = UserRepositoryImpl { pool: pool };
+            assert_eq!(1, result.count);
 
-        let result = repo.count_by_user_id(&UserID::new("1".to_string())).await?;
-        assert_eq!(result, 1);
+            cleaner.clean().await?;
 
-        cleaner.clean().await?;
-
-        Ok(())
+            Ok(())
+        }
     }
 
-    #[tokio::test]
-    async fn test_user_repository_count_without_result() -> Result<()> {
-        let pool = get_db_connection_for_test().await;
-        let mut cleaner = Cleaner::new(pool.clone());
-        cleaner.prepare_table("user").await?;
-        let repo = UserRepositoryImpl { pool: pool };
+    mod count_by_user_id {
+        use crate::database::get_db_connection_for_test;
+        use crate::models::user::UserID;
+        use crate::repos::user_repository::{UserRepository, UserRepositoryImpl};
+        use crate::repos::Result;
+        use crate::test::Cleaner;
 
-        let result = repo.count_by_user_id(&UserID::new("1".to_string())).await?;
-        assert_eq!(0, result);
+        #[tokio::test]
+        async fn with_result() -> Result<()> {
+            let pool = get_db_connection_for_test().await;
+            let mut cleaner = Cleaner::new(pool.clone());
+            cleaner.prepare_table("user").await?;
 
-        cleaner.clean().await?;
+            sqlx::query!("INSERT INTO user (jia_user_id) VALUES (1)")
+                .execute(&pool)
+                .await?;
 
-        Ok(())
+            let repo = UserRepositoryImpl { pool: pool };
+
+            let result = repo.count_by_user_id(&UserID::new("1".to_string())).await?;
+            assert_eq!(result, 1);
+
+            cleaner.clean().await?;
+
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn without_result() -> Result<()> {
+            let pool = get_db_connection_for_test().await;
+            let mut cleaner = Cleaner::new(pool.clone());
+            cleaner.prepare_table("user").await?;
+            let repo = UserRepositoryImpl { pool: pool };
+
+            let result = repo.count_by_user_id(&UserID::new("1".to_string())).await?;
+            assert_eq!(0, result);
+
+            cleaner.clean().await?;
+
+            Ok(())
+        }
     }
 }
