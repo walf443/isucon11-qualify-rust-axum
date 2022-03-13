a97b0663d3e65d0700207dcdbd122739818b1862
Keiji Yoshimi <walf443@gmail.com>
2022-03-13 16:59:52 +0900

    added test case when isu_list_service failed

--- isucondition-http/src/routes/isu_routes/tests/get_isu_list.rs
+++ isucondition-http/src/routes/isu_routes/tests/get_isu_list.rs
@@ -2,6 +2,7 @@ use crate::get_isu_list;
 use crate::requests::current_user_id::CurrentUserID;
 use axum::extract::Extension;
 use isucondition_core::models::user::UserID;
+use isucondition_core::repos;
 use isucondition_core::repos::repository_manager::tests::MockRepositoryManager;
 use isucondition_core::services::service_manager::tests::MockServiceManager;
 use isucondition_core::services::service_manager::{ServiceManager, ServiceManagerImpl};
@@ -19,7 +20,25 @@ async fn no_sigined_in() -> () {
 }
 
 #[tokio::test]
-async fn signed_in() -> Result<(), anyhow::Error> {
+#[should_panic(expected = "ReposError")]
+async fn signed_in_and_isu_list_service_returning_error() -> () {
+    let repo = Arc::new(MockRepositoryManager::new());
+    let mut service = MockServiceManager::new(repo);
+    let user_id = UserID::new("1".to_string());
+
+    service
+        .isu_list_service
+        .expect_run()
+        .returning(|_user_id| Err(repos::Error::TestError()));
+
+    let ext_service = Extension(Arc::new(service));
+    get_isu_list(ext_service, CurrentUserID::Some(user_id.clone()))
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn signed_in_and_isu_list_service_returning_empty() -> Result<(), anyhow::Error> {
     let repo = Arc::new(MockRepositoryManager::new());
     let mut service = MockServiceManager::new(repo);
     let user_id = UserID::new("1".to_string());
